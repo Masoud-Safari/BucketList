f6001305620870a7b22c2723f35be37569095664
Masoud Safari <mmsafari@gmail.com>
2024-03-13 15:17:14 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Masoud Safari on 2024-03-13.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
