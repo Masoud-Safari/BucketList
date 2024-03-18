9ecc4ebe9148d28ad57c64ce1b0f2b4ae8ea450e
Masoud Safari <mmsafari@gmail.com>
2024-03-18 11:46:53 -0700

    Add a picker to change map style.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -18,35 +18,69 @@ struct ContentView: View {
     
     @State private var viewModel = ViewModel()
     
+    let mapStyleList = ["Default", "Satellite", "Hybrid"]
+    @State private var selectedMapStyle = "Default"
+    
+    var mapStyle: MapStyle {
+        switch selectedMapStyle {
+        case "Satellite":
+            return .imagery
+        case "Hybrid":
+            return .hybrid
+        default:
+            return .standard
+        }
+    }
+    
     var body: some View {
         if viewModel.isUnlocked {
-            MapReader { proxy in
-                Map(initialPosition: startPosition) {
-                    ForEach(viewModel.locations) { location in
-                        Annotation(location.name, coordinate: location.coordinate) {
-                            Image(systemName: "star.circle")
-                                .resizable()
-                                .foregroundStyle(.red)
-                                .frame(width: 44, height: 44)
-                                .background(.white)
-                                .clipShape(.circle)
-                                .onLongPressGesture {
-                                    viewModel.selectedPlace = location
-                                }
+            ZStack {
+                MapReader { proxy in
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            Annotation(location.name, coordinate: location.coordinate) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44, height: 44)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .onLongPressGesture {
+                                        viewModel.selectedPlace = location
+                                    }
+                            }
                         }
                     }
-                }
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        viewModel.addLocation(at: coordinate)
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            viewModel.addLocation(at: coordinate)
+                        }
                     }
-                }
-                .sheet(item: $viewModel.selectedPlace) { place in
-                    EditView(location: place) {
-                        viewModel.update(location: $0)
-                    } onDelete: { place in
-                        viewModel.deleteLocation(location: place)
+                    .sheet(item: $viewModel.selectedPlace) { place in
+                        EditView(location: place) {
+                            viewModel.update(location: $0)
+                        } onDelete: { place in
+                            viewModel.deleteLocation(location: place)
+                        }
                     }
+                    .mapStyle(mapStyle)
+                }
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        
+                        Picker("Map Style", selection: $selectedMapStyle) {
+                            ForEach(mapStyleList, id: \.self) { style in
+                                Text("\(style)")
+                            }
+                        }
+                        .background(Color(UIColor.systemBackground).opacity(0.7))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .padding()
+                    }
+                    
+                    Spacer()
                 }
             }
         } else {
